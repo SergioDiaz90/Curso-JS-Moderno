d17b5ef0b1fd6e093d158bdb24402e4f84f0f81b
S3RG1O1994 <se.diaz60@gmail.com>
2020-10-17 01:36:14 -0500

    [Feature]: Add validation inputs form

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -1,10 +1,18 @@
 // variables
 const sendButton = document.querySelector('#enviar');
+// inputs text
+const emailInput = document.querySelector('#email');
+const subjectInput = document.querySelector('#asunto');
+const messageInput = document.querySelector('#mensaje');
 
 eventListeners();
 function eventListeners() {
 	document.addEventListener('DOMContentLoaded', runApp);
 	
+	// textarea inputs form
+	emailInput.addEventListener('blur', validateForm);
+	subjectInput.addEventListener('blur', validateForm);
+	messageInput.addEventListener('blur', validateForm);
 }
 
 
@@ -12,4 +20,13 @@ function eventListeners() {
 function runApp (e) {
 	// deshabilidar boton
 	sendButton.disabled = true;
+}
+
+function validateForm (e) {
+	if (e.target.value.length > 0) {
+		console.log('Si tengo algo');
+	} else {
+		// emailInput.style.borderBottomColor = 'red';
+		e.target.classList.add('border', 'border-red-500');
+	}
 }
