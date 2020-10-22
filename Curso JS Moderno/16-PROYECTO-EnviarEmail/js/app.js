1ab2e52eb3d23360c96f42e03a9ef88756365756
S3RG1O1994 <se.diaz60@gmail.com>
2020-10-22 18:07:13 -0500

    [Update]: Validation completed

--- Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
+++ Curso JS Moderno/16-PROYECTO-EnviarEmail/js/app.js
@@ -5,7 +5,8 @@ const sendMail = document.querySelector('#enviar-mail');
 const emailInput = document.querySelector('#email');
 const subjectInput = document.querySelector('#asunto');
 const messageInput = document.querySelector('#mensaje');
-
+// Regex expresion
+const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 /** function for despatch events*/
 eventListeners();
 function eventListeners() {
@@ -31,21 +32,28 @@ function validateForm (e) {
 				e.target.classList.add('border', 'border-red-500');
 				seeError('This field is required');
 				break;
-			} else if (e.target.value.indexOf('@') < 0) {
+			} else if (!regex.test(emailInput.value)) {
 				e.target.classList.add('border', 'border-red-500');
 				seeError('this email is not valid');
 				break;
-			} 
+			} else {
+				removeError(e.target);
+			}
 		case 'text':
 			if (e.target.value.length === 0) {
+				e.target.classList.remove('border', 'border-green-500');
 				e.target.classList.add('border', 'border-red-500');
 				seeError('this field cannot be empty');
+			} else {
+				removeError(e.target);
 			}
 			break;
 		case 'textarea':
 			if (e.target.value.length === 0) {
 				e.target.classList.add('border', 'border-red-500');
 				seeError('this field cannot be empty');
+			} else {
+				removeError(e.target);
 			}
 			break;
 	};
@@ -63,5 +71,18 @@ function seeError(msn) {
 	if (error.length === 0){
 		// sendMail.appendChild(messageError);
 		sendMail.insertBefore(messageError, document.querySelector('.mb-10'));
+	}
+}
+
+function removeError (msn) {
+	try {
+		const err = document.querySelector('.error');
+		err.remove();
+	} catch (error) {
+		msn.classList.remove('border', 'border-red-500');
+		msn.classList.add('border', 'border-green-500');
+	} finally {
+		msn.classList.remove('border', 'border-red-500');
+		msn.classList.add('border', 'border-green-500');
 	}
 }
